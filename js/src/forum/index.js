b4c7dcff94f5edfe90b96f00fde82f6aecd11f1a
Justoverclock <info@justoverclock.it>
2021-08-06 13:17:34 +0200

    2nd attempt

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,8 @@
 import app from 'flarum/app';
+import {extend} from 'flarum/common/extend';
 import addAds from "./addAds";
+import CommentPost from "flarum/forum/components/CommentPost";
 
 app.initializers.add('justoverclock/adsense-manager', () => {
-  addAds();
+  extend(CommentPost.prototype, 'headerItems', addAds);
 });
